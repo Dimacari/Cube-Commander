23cac32b5e16969445dd60e245b57c64b3cdce33
Coen Romanowski <coenromo@gmail.com>
2015-04-28 20:21:08 +1000

    Added sound assets

---
+++ cube-commander-unity-4/Assets/scripts/BackgroundMusic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour {
+	
+	public AudioClip ambient_noise;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void ambient_noisePlay () {
+		audio.PlayOneShot (ambient_noise);
+	}
+	
+}
